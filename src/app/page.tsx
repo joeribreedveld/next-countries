47d0fa9bb166354252d1ef5b3cb6c8a7ed12d163
joeribreedveld <joeri.breedveld123@gmail.com>
2024-11-27 11:53:53 +0100

    fix: add internal api to prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,9 @@
 import Question from "@/components/question";
+import { getQuestions } from "@/lib/actions";
 import { Suspense } from "react";
 
 export default async function Home() {
-  // const questions = await getQuestions();
-
-  const questions = [
-    {
-      answer: {
-        name: {
-          common: "Afghanistan",
-        },
-        flags: {
-          svg: "https://restcountries.com/data/afg.svg",
-        },
-      },
-      choices: [
-        {
-          name: {
-            common: "Afghanistan",
-          },
-          flags: {
-            svg: "https://restcountries.com/data/afg.svg",
-          },
-        },
-        {
-          name: {
-            common: "Albania",
-          },
-          flags: {
-            svg: "https://restcountries.com/data/alb.svg",
-          },
-        },
-        {
-          name: {
-            common: "Algeria",
-          },
-          flags: {
-            svg: "https://restcountries.com/data/dza.svg",
-          },
-        },
-        {
-          name: {
-            common: "Andorra",
-          },
-          flags: {
-            svg: "https://restcountries.com/data/and.svg",
-          },
-        },
-      ],
-    },
-  ];
+  const questions = await getQuestions();
 
   return (
     <main className="container mx-auto max-w-2xl px-4 py-16 sm:py-24">
